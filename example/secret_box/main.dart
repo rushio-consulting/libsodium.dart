555a796307c4ff4205e35e21bd959dc338b36138
kevin <segaud.kevin@gmail.com>
2019-04-25 18:47:52 +0200

    event easier when using a password

--- example/secret_box/main.dart
+++ example/secret_box/main.dart
@@ -9,17 +9,17 @@ void main() {
   final secretBox = SecretBox();
   final key = secretBox.keygen();
   // final nonce = utils.randomBytesBuf(secretBox.nonceLenght);
-  final nonce = Digest('monsuperpassword'.codeUnits);
+  final nonce = 'monsuperpassword'.codeUnits;
   final message = 'Hello world !';
   print('key = ${key.bytes}');
   print('nonce = ${nonce.bytes}');
   print('messageLength = ${message.length}');
 
-  final cipherText = secretBox.easy(message.codeUnits, nonce.bytes, key.bytes);
+  final cipherText = secretBox.easy(message.codeUnits, nonce, key.bytes);
   print('cipherText = $cipherText');
 
-  final decryptedText = secretBox.openEasy(
-      message.length, cipherText.bytes, nonce.bytes, key.bytes);
+  final decryptedText =
+      secretBox.openEasy(message.length, cipherText.bytes, nonce, key.bytes);
   final decryptedMessage = utf8.decode(decryptedText.bytes);
   print('decryptedMessage = $decryptedMessage');
 }
